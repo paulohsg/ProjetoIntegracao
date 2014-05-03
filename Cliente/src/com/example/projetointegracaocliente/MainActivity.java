cfd72b699631e5682c80467a03b70a6d602d6ba3
paulo <paulo@>
2014-05-03 14:40:54 -0300

    Criação do método "checkPlayServices"

--- Cliente/src/com/example/projetointegracaocliente/MainActivity.java
+++ Cliente/src/com/example/projetointegracaocliente/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.projetointegracaocliente;
 
+import static android.support.v4.app.FragmentActivity.TAG;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
 public class MainActivity extends ActionBarActivity {
 
+	
+	public static final String EXTRA_MESSAGE = "message";
+    public static final String PROPERTY_REG_ID = "registration_id";
+    private static final String PROPERTY_APP_VERSION = "appVersion";
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    
+    static final String TAG = "GCM Demo";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
@@ -35,6 +48,26 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+	
+	@Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 	
 	private static int getAppVersion(Context context) {
         try {
