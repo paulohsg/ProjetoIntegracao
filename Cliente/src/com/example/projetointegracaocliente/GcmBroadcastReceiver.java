744cd8bd7c0e7bce872080760f7e11d8a832da8f
paulohsg <paulohenrique_sg@hotmail.com>
2014-06-19 01:58:41 -0300

    Adicionado GcmBroadcastReceiver

--- Cliente/src/com/example/projetointegracaocliente/GcmBroadcastReceiver.java
+++ Cliente/src/com/example/projetointegracaocliente/GcmBroadcastReceiver.java
@@ -1,9 +1,21 @@
 package com.example.projetointegracaocliente;
 
-public class GcmBroadcastReceiver {
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
 
-	public GcmBroadcastReceiver() {
-		// TODO Auto-generated constructor stub
-	}
-
+public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
+	
+	
+    @Override
+    public void onReceive(Context context, Intent intent) {
+       
+        ComponentName comp = new ComponentName(context.getPackageName(),
+        		GcmIntentService.class.getName());
+        
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+    }
 }
