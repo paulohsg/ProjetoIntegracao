b81efb62c74375f7a07baa2e33b47a0a3da03309
paulohsg <paulohenrique_sg@hotmail.com>
2014-06-19 01:57:44 -0300

    Adicionado GcmIntentService

--- Cliente/src/com/example/projetointegracaocliente/GcmIntentService.java
+++ Cliente/src/com/example/projetointegracaocliente/GcmIntentService.java
@@ -1,9 +1,57 @@
 package com.example.projetointegracaocliente;
 
-public class GcmIntentService {
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+public class GcmIntentService extends IntentService {
+
+    String mes;
+    private Handler handler;
+    public static final String EXTRA_MESSAGE = "mensagem";
 	public GcmIntentService() {
-		// TODO Auto-generated constructor stub
+		super("GcmIntentService");
 	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		handler = new Handler();
+	}
+	@Override
+	protected void onHandleIntent(Intent intent) {
+       Bundle extras = intent.getExtras();
+       
+       GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+      
+       String messageType = gcm.getMessageType(intent);
+
+      mes = extras.getString(EXTRA_MESSAGE);
+      mostrarToast();
+      Log.i("GCM", "Recebido : (" +messageType+")  "+extras.getString(EXTRA_MESSAGE));
+      
+             
+       
+       GcmBroadcastReceiver.completeWakefulIntent(intent);
+
+	}
+	
+	public void mostrarToast(){
+		handler.post(new Runnable() {
+		    public void run() {
+		        Toast.makeText(getApplicationContext(),mes , Toast.LENGTH_LONG).show();
+		    }
+		 });
+
+	}
+	
+	
+	
+	
 
 }
